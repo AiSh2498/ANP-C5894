884d4900ab50afc042a1d87614772a81eac11260
AiSh2498 <137864775+AiSh2498@users.noreply.github.com>
2023-06-30 13:59:23 +0530

    Add files via upload

---
+++ Hands-on/HelloEarthlings.java
@@ -0,0 +1,10 @@
+//to print simple "Hello Earthlings"
+class HelloEarthlings
+{
+public static void main (String[] args)
+{
+System.out.println("Hello Earthlings");
+
+System.out.println("Greetings from outer space");
+}
+}
