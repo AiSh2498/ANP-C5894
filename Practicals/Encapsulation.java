c1bf60aa2f528c557c44f2f93869dcac1044fe1a
AiSh2498 <137864775+AiSh2498@users.noreply.github.com>
2023-07-04 13:58:50 +0530

    Add files via upload

---
+++ Practicals/Encapsulation.java
@@ -0,0 +1,37 @@
+package oops;
+// example to learn encapsulation
+ class Encapsulation
+ {
+	 private String name;
+	 private int age;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	 
+	 
+}
+class Main
+{
+	public static void main(String[] args)
+	{
+		//Creating instance of class Encapsulation
+		Encapsulation e=new Encapsulation();
+		
+		//setting values
+		e.setName("Aishu");
+		e.setAge(23);
+		
+		//print values
+		System.out.println("Name is:" + e.getName());
+		System.out.println("Age is:" + e.getAge());
+	}
+}
