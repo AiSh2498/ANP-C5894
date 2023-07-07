58ac400a82dc2df506dc7d3ad61b0c12afe877d1
AiSh2498 <137864775+AiSh2498@users.noreply.github.com>
2023-07-07 13:45:16 +0530

    Add files via upload

--- Hands-on/Shape.java
+++ Hands-on/Shape.java
@@ -1,31 +1,81 @@
 package oops;
-class Shape
+
+abstract class Shape 
 {
-	String name;
-	public void calculateArea() 
-	{
-	System.out.println("Calculating area of Shape");
-	}
+	public abstract void calculateArea();
+	public abstract void calculatePerimeter();
 
 }
-
-class Rectangle extends Shape
+class Rectangles extends Shape
+{
+//	private int l;
+//	private int b;
+//	
+//	public int getL() {
+//		return l;
+//	}
+//	public void setL(int l) {
+//		this.l = l;
+//	}
+//	public int getB() {
+//		return b;
+//	}
+//	public void setB(int b) {
+//		this.b = b;
+//	}
+	public void calculateArea()
+	{
+		System.out.println("Area of rectangle is: l*b");
+	}
+	public void calculatePerimeter()
+	{
+		System.out.println("Perimeter of rectangle: 2(l+b)");
+	}
+	
+}
+class Circles extends Shape
 {
 	public void calculateArea()
 	{
-		System.out.println("Area of rectangle");
+		System.out.println("Area of circle is: πr^2 ");
 	}
+	public void calculatePerimeter()
+	{
+		System.out.println("Perimeter of circle is: 2πr");
+	}
+	
 }
-class ShapeMain
+class Triangles extends Shape
+{
+	public void calculateArea()
+	{
+		System.out.println("Area of Triangle is: (lxb)/2");
+	}
+	public void calculatePerimeter()
+	{
+		System.out.println("Perimeter of Triangle is: a+b+c ");
+	}
+	
+}
+class Shapes_Main
 {
 	public static void main(String[] args)
 	{
-		//creating object of sub class
-		Rectangle d=new Rectangle();
-		//access fields and methods
-		//access fields and methods
-				d.calculateArea();
-			}
-
+		Rectangles k=new Rectangles();
+		k.calculateArea();
+		k.calculatePerimeter();
+		
+		Circles b=new Circles();
+		b.calculateArea();
+		b.calculatePerimeter();
+		
+		Triangles c=new Triangles();
+		c.calculateArea();
+		c.calculatePerimeter();
+	
+		
+       
+		
+		
+	}
 }
-
